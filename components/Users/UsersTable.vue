b65199aaea4b51252c42e2805b5b7c9c2c0ac9df
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-07-07 00:32:23 +0100

    hafiz edited

---
+++ components/Users/UsersTable.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="w-full">
+    <div
+      class="flex flex-row justify-center md:justify-between w-full gap-4 mb-5"
+    >
+      <USelect
+        :items="dateOptions"
+        v-model="dateOption"
+        class="text-black-300"
+        name="Sort"
+        placeholder="Sort"
+      />
+      <USelect
+        :items="creators"
+        v-model="selectedCreator"
+        class="text-black-300"
+        placeholder="Creators"
+      />
+      <UInputNumber
+        v-model="limit"
+        orientation="vertical"
+        placeholder="Limit"
+        class="text-black-300"
+      />
+    </div>
+    <UsersTableModel
+      :columns0="columns"
+      :data0="users"
+      :creators-lst="creators"
+      :key="users.length"
+    />
+    <UButtonGroup class="mt-5">
+      <UButton
+        color="neutral"
+        variant="outline"
+        label="Prev"
+        @click="page > 0 ? page-- : {}"
+      />
+      <UButton color="neutral" variant="outline" label="Next" @click="page++" />
+    </UButtonGroup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { TableColumn } from "@nuxt/ui";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+const props = defineProps({
+  creators: { type: Array, required: true },
+  supabaseClient: { type: SupabaseClient, required: true },
+  userType: { type: String, required: true },
+});
+
+// initialize variables
+const users = ref([]);
+const dateOptions = ["asc", "desc"];
+const dateOption = ref(dateOptions[0]);
+const selectedCreator = ref(props.creators[0]);
+const limit = ref(10);
+const page = ref(1);
+const tableLoading: Ref<boolean> = ref(false);
+
+const cols: string[] = ["id", "user_id", "username", "full_name", "assigned"];
+const columns: TableColumn<any>[] = [];
+
+// setting up the columns
+cols.forEach((col) => {
+  let keyName = "";
+  let cell = "";
+  if (col == "id") {
+    keyName = "#";
+    cell = "#";
+  } else {
+    keyName = col.replace("_", " ").toUpperCase();
+    cell = "";
+  }
+  columns.push({
+    accessorKey: col,
+    header: keyName,
+    cell: ({ row }) => `${cell}${row.getValue(col)}`,
+    meta: {
+      class: {
+        th: "text-red-900 text-center",
+        td: "text-gray-800",
+      },
+    },
+  });
+});
+
+// Functions
+const loadUsers = async (userType: string) => {
+  tableLoading.value = true;
+  console.log(selectedCreator.value, limit.value, page.value);
+  // loadingData.value = true;
+  if (page.value == 0) {
+    page.value = 1;
+  }
+  const offset = limit.value * (page.value - 1);
+  if (!props.supabaseClient) return [];
+  try {
+    let stmt = props.supabaseClient
+      .from("users")
+      .select("*")
+      .limit(limit.value)
+      .range(offset, offset + limit.value);
+    if (selectedCreator.value && userType == "unassigned") {
+      stmt = stmt.eq("assigned", selectedCreator.value);
+    } else if (userType == "unTreated") {
+      stmt.or(`last_interaction_date.is.null`);
+    }
+    const { data } = await stmt;
+    users.value = data;
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    tableLoading.value = false;
+  }
+};
+
+// monitor changes in a few sources
+watch(
+  () => [selectedCreator.value, limit.value, page.value],
+  async ([newCreator, newLimit, newPage], [oldCreator, oldLimit, oldPage]) => {
+    await loadUsers(props.userType);
+  },
+);
+
+// load some data when component loads
+onMounted(async () => {
+  await loadUsers(props.userType);
+});
+</script>
---
+++ pages/users.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="min-h-screen flex">
+    <!-- Main Content -->
+    <div class="flex-1 flex flex-col overflow-hidden w-full">
+      <!-- Header -->
+      <header class="bg-white shadow-sm py-4 px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-center">
+          <!-- Mobile Menu Button -->
+          <h3 class="text-2xl font-semibold text-center">
+            User/Scraper Dashboard
+          </h3>
+        </div>
+      </header>
+      <!-- Main Content -->
+      <main class="text-center overflow-y-auto md:p-4 p-1 lg:p-8">
+        <!-- Welcome -->
+        <div class="flex justify-center">
+          <div class="flex flex-col ml-4">
+            <span class="text-2xl md:text-3xl m-5">
+              Welcome, <span class="font-bold text-primary-800"> Here </span> !
+            </span>
+          </div>
+        </div>
+        <!-- Page Content -->
+        <!-- Loading -->
+        <div
+          v-if="loadingData"
+          class="place-items-center place-content-center my-15"
+        >
+          <UProgress animation="carousel" color="secondary" />
+        </div>
+        <!-- When Loaded -->
+        <div v-else class="mt-10">
+          <div v-for="variation in variations" class="mb-10">
+            <!-- Unassigned Card  -->
+            <UCollapsible :default-open="true">
+              <UButton
+                class="w-full p-4 bg-gray-300 rounded-bl-none rounded-br-none"
+              >
+                <span class="font-bold text-sm md:text-lg">
+                  {{ variation.title }}
+                </span>
+              </UButton>
+              <template #content>
+                <UCard class="bg-gray-100 w-full">
+                  <UsersTable
+                    :creators="creatorsLst"
+                    :supabase-client="supabase"
+                    :user-type="variation.userType"
+                  />
+                </UCard>
+              </template>
+            </UCollapsible>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { resolveComponent } from "vue";
+
+const UButton = resolveComponent("UButton");
+
+// Initialise supabase client
+const { supabase } = useSupabaseSub();
+
+// declare variables
+const creatorsLst: Ref<any[]> = ref([]);
+const loadingData: Ref<boolean> = ref(false);
+const variations: Array<object> = [
+  {
+    userType: "Unassigned",
+    title: "Unassigned Users",
+  },
+  {
+    userType: "Untreated",
+    title: "Untreated Users",
+  },
+];
+
+// Functions
+async function loadCreators() {
+  loadingData.value = true;
+  try {
+    let { data } = await supabase.from("creators").select("*");
+    creatorsLst.value = data?.map((d) => d.creator) ?? [];
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    loadingData.value = false;
+  }
+}
+onMounted(async () => {
+  await loadCreators();
+});
+</script>
