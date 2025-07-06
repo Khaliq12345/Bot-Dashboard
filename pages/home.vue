e3a1bd5c9d2a4a5fa20559b3ea029861ca0f91aa
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-07-07 00:50:22 +0100

    home page added

--- pages/home.vue
+++ pages/home.vue
@@ -1,21 +1,32 @@
 <template>
-    <ClientOnly>
-        <div
-            class="min-h-screen text-center bg-gradient-to-br from-red-50 to-gray-50 flex items-center justify-center p-4">
-            <UCard class="w-full rounded-2xl p-5 max-w-md shadow-2xl" variant="solid">
-                <div class="text-center space-y-3">
-                    <UIcon size="50" name="i-lucide-home" class="w-12 h-12 mx-auto text-primary-500" />
-                    <h1 class="text-3xl font-bold ">Home Page</h1>
-                    <p class="text-gray-500">Navigation</p>
-                </div>
-                <UButton to="/users" color="neutral" class="w-full p-4 bg-primary-200 mt-10 rounded-2xl">
-                    <span class="font-bold text-xl"> Users / Scrape </span>
-                </UButton>
+  <div
+    class="min-h-screen text-center bg-gradient-to-br from-red-50 to-gray-50 flex items-center justify-center p-4"
+  >
+    <UCard class="w-full rounded-2xl p-5 max-w-md shadow-2xl" variant="solid">
+      <div class="text-center space-y-3">
+        <UIcon
+          size="50"
+          name="i-lucide-home"
+          class="w-12 h-12 mx-auto text-primary-500"
+        />
+        <h1 class="text-3xl font-bold">Home Page</h1>
+        <p class="text-gray-500">Navigation</p>
+      </div>
+      <UButton
+        to="/users"
+        color="neutral"
+        class="w-full p-4 bg-primary-200 mt-10 rounded-2xl"
+      >
+        <span class="font-bold text-xl"> Users / Scrape </span>
+      </UButton>
 
-                <UButton to="/bots" color="neutral" class="w-full p-4 bg-primary-200 mt-10 rounded-2xl">
-                    <span class="font-bold text-xl"> Bot Status </span>
-                </UButton>
-            </UCard>
-        </div>
-    </ClientOnly>
+      <UButton
+        to="/bots"
+        color="neutral"
+        class="w-full p-4 bg-primary-200 mt-10 rounded-2xl"
+      >
+        <span class="font-bold text-xl"> Bot Status </span>
+      </UButton>
+    </UCard>
+  </div>
 </template>
