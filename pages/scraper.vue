aa0b2b68140c6b531f4303913abb44d6cea53b40
Hafiz <abdulmoustapha64@@gmail.com>
2025-07-09 23:27:20 +0100

    Worked out Scraper

--- pages/scraper.vue
+++ pages/scraper.vue
@@ -1,3 +1,163 @@
 <template>
-    s
+    <DashBoardHeader title="Scraper Dashboard">
+        <div v-if="loadingData || !botCard" class="place-items-center place-content-center my-15">
+            <UProgress animation="carousel" color="secondary" />
+        </div>
+        <!-- When Loaded -->
+        <div v-else class="mt-10">
+            <!-- Bot Card -->
+            <div class="mb-10 flex flex-col md:flex-row gap-5 items-center justify-center">
+                <UCard class="w-full rounded-2xl max-w-md shadow-2xl md:text-lg text-sm" variant="outline" :class="{
+                    'bg-gray-300': !botCard.id,
+                    'bg-green-300': botCard.id && botCard.status == 'success',
+                    'bg-red-300': botCard.id && botCard.status == 'failed',
+                }">
+                    <div class="text-center">
+                        <h1 class="text-2xl font-bold uppercase mb-1">{{ botCard.status }}</h1>
+                        <p class="text-gray-500">Last Run Status</p>
+                    </div>
+                    <USeparator class="mr-5 my-3" />
+                    <div v-if="botCard.id" class="text-left">
+                        <div class="mb-2" v-for="col in columns">
+                            <div v-if="botCard[col.key]" class="">
+                                <span class="font-bold">{{ col["title"] }}: </span>
+                                <span class="ml-2">
+                                    {{ botCard[col["key"]] }}
+                                </span>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div v-else class="py-15 font-bold">No Data</div>
+                </UCard>
+            </div>
+            <!--  -->
+            <USeparator class="mb-10" />
+            <!--  -->
+            <!-- General Metrics -->
+            <div v-if="generalMetricskeys" class="mb-10 flex flex-wrap gap-2 md:gap-5 items-center justify-center">
+                <div class="" v-for="item in generalMetrics">
+                    <UCard variant="soft" class="text-center shadow-2xl" :class="item.color">
+                        <div class="flex items-center justify-start">
+                            <div class="text-left">
+                                <UIcon :name="item.icon" size="30" class="mr-5" />
+                                <h3 class="text-md md:text-lg font-bold mb-2">
+                                    {{ generalMetricskeys[item['key']] }}
+                                </h3>
+                                <p class="font-semibold">{{ item.title }}</p>
+                            </div>
+                        </div>
+                    </UCard>
+                </div>
+            </div>
+        </div>
+    </DashBoardHeader>
 </template>
+
+<script setup lang="ts">
+import type { SupabaseClient } from "@supabase/supabase-js";
+
+// Initialise supabase client
+const supabase = inject("supabase") as SupabaseClient;
+
+// declare variables
+const loadingData: Ref<boolean> = ref(false);
+const botCard: Ref<any> = ref(null);
+const columns: any[] = [
+    {
+        title: "Created At",
+        key: "created_at",
+    },
+    {
+        title: "Error Details",
+        key: "error",
+    },
+];
+const generalMetricskeys: Ref<any> = ref({
+    totalRunning: 0,
+    totalSuccess: 0,
+    totalFails: 0,
+    successRate: '',
+    failRate: '',
+})
+const generalMetrics = [
+    {
+        title: "Total Running",
+        icon: "i-lucide-drone",
+        key: "totalRunning",
+        color: "bg-amber-200",
+    },
+    {
+        title: "Total Success",
+        icon: "i-lucide-check-check",
+        key: "totalSuccess",
+        color: "bg-green-200",
+    },
+    {
+        title: "Total Fails",
+        icon: "i-lucide-circle-x",
+        key: "totalFails",
+        color: "bg-red-200",
+    },
+    {
+        title: "Success Rate",
+        icon: "i-lucide-square-percent",
+        key: "successRate",
+        color: "bg-green-200",
+    },
+    {
+        title: "Fail Rate",
+        icon: "i-lucide-diamond-percent",
+        key: "failRate",
+        color: "bg-red-200",
+    },
+];
+
+// Functions
+async function loadCardsInfo() {
+    console.log("Calling");
+    loadingData.value = true;
+    try {
+        let { data, error } = await supabase
+            .from("scraping_status")
+            .select("*")
+            .order("id", { ascending: false })
+            // .eq('id', '3')
+            .limit(1);
+        if (data && data.length > 0) {
+            botCard.value = data[0];
+        }
+        // Working Metrics
+        const { data: history, error: historyError } = await supabase
+            .from("scraping_status")
+            .select("*");
+
+        if (history) {
+            const totalRuns = history.length;
+            const successes = history.filter(
+                (x) => x.status === "success",
+            ).length;
+            const failures = history.filter((x) => x.status === "failed").length;
+            const successrate = totalRuns ? (successes / totalRuns) * 100 : 0;
+            const failurerate = totalRuns ? (failures / totalRuns) * 100 : 0;
+            const running = history.filter((x) => x.status === "running").length;
+            generalMetricskeys.value = {
+                totalRunning: running,
+                totalSuccess: successes,
+                totalFails: failures,
+                successRate: `${successes}/${totalRuns} (${Number(successrate.toFixed(2))} %)`,
+                failRate: `${failures}/${totalRuns} (${Number(failurerate.toFixed(2))} %)`,
+            }
+        }
+    } catch (err) {
+        console.error("Error:", err);
+    } finally {
+        loadingData.value = false;
+    }
+}
+
+onMounted(async () => {
+    await loadCardsInfo();
+});
+</script>
--- pages/users.vue
+++ pages/users.vue
@@ -1,34 +1,50 @@
 <template>
   <DashBoardHeader title="User/Scraper Dashboard">
-    <div
-      v-if="loadingData"
-      class="place-items-center place-content-center my-15"
-    >
+    <div v-if="loadingData" class="place-items-center place-content-center my-15">
       <UProgress animation="carousel" color="secondary" />
     </div>
     <!-- When Loaded -->
     <div v-else class="mt-10">
+      <div class="justify-center items-center">
+        <UButtonGroup class="">
+          <USelect :items="creatorsLst" v-model="creator" name="Creators" placeholder="Creators" class="text-red-300"
+            :required="false" />
+          <UButton @click="creator = ''" color="neutral" icon="i-heroicons-x-mark" />
+        </UButtonGroup>
+
+
+        <div class="mt-5 mb-10 flex gap-2 md:gap-5 items-center justify-center">
+          <UCard v-for="card in topCards" variant="soft" class="text-center shadow-2xl">
+            <div class="flex items-center justify-start">
+              <div class="text-left">
+                <UIcon :name="card.icon" size="30" class="mr-5" />
+                <h3 class="text-md md:text-lg font-bold mb-2">
+                  {{ topCardsKeys[card['key']] }}
+                </h3>
+                <p class="font-semibold">{{ card.title }}</p>
+              </div>
+            </div>
+          </UCard>
+        </div>
+      </div>
+      <!-- Tables -->
       <div v-for="variation in variations" class="mb-10">
         <!-- Unassigned Card  -->
         <UCollapsible :default-open="true">
-          <UButton
-            class="w-full p-4 bg-gray-300 rounded-bl-none rounded-br-none"
-          >
+          <UButton class="w-full p-4 bg-gray-300 rounded-bl-none rounded-br-none">
             <span class="font-bold text-sm md:text-lg">
               {{ variation.title }}
             </span>
           </UButton>
           <template #content>
             <UCard class="bg-gray-100 w-full">
-              <UsersTable
-                :creators="creatorsLst"
-                :user-type="variation.userType"
-              />
+              <UsersTable :creators="creatorsLst" :user-type="variation.userType" />
             </UCard>
           </template>
         </UCollapsible>
       </div>
     </div>
+
   </DashBoardHeader>
 </template>
 
@@ -40,6 +56,7 @@ const supabase = inject("supabase") as SupabaseClient;
 
 // declare variables
 const creatorsLst: Ref<any[]> = ref([]);
+const creator: Ref<any> = ref(null);
 const loadingData: Ref<boolean> = ref(false);
 const variations: Array<any> = [
   {
@@ -49,6 +66,22 @@ const variations: Array<any> = [
   {
     userType: "Untreated",
     title: "Untreated Users",
+  },
+];
+const topCardsKeys: Ref<any> = ref({
+  total_assigned: 0,
+  total_untreated: 0,
+})
+const topCards: Array<any> = [
+  {
+    key: "total_assigned",
+    icon: "i-lucide-user-round-check",
+    title: "Total Assigned",
+  },
+  {
+    key: "total_untreated",
+    icon: "i-lucide-user-round-x",
+    title: "Total Untreated",
   },
 ];
 
@@ -64,7 +97,34 @@ async function loadCreators() {
     loadingData.value = false;
   }
 }
+const loadStats = async () => {
+  loadingData.value = true;
+  try {
+    let stmt = supabase
+      .from("users")
+      .select("*")
+    if (creator.value) {
+      stmt = stmt.eq("assigned", creator.value);
+    }
+    const { data } = await stmt;
+    console.log("data Loaded -- ", data?.length)
+    topCardsKeys.value = {
+      total_assigned: data?.filter((v) => v.assigned).length,
+      total_untreated: data?.filter((v) => !v.last_interaction_date).length,
+    };
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    loadingData.value = false;
+  }
+};
+
+watch(creator, async (newVal, oldVal) => {
+  await loadStats();
+});
+
 onMounted(async () => {
   await loadCreators();
+  await loadStats();
 });
 </script>
